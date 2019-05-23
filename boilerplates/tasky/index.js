b623dbd1a0ee6682c7738828d7c3ae4307bdb20c
Prokopenko Ivan <ivan.prokopenko@together.com>
2019-05-23 15:12:31 +0300

    Move browser logic to the own class MainWindow

--- boilerplates/tasky/index.js
+++ boilerplates/tasky/index.js
@@ -2,26 +2,17 @@ const path = require('path');
 const electron = require('electron');
 const { app, BrowserWindow, Tray } = electron;
 const TimerTray = require('./app/timer_tray');
+const MainWindow = require('./app/main_window');
 
 let mainWindow;
 let tray;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        height: 500,
-        width: 300,
-        frame: false,
-        resizable: false,
-        show: false
-    });
-
-    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+    app.dock.hide();
+    mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);
+    
     const iconName = process.platform === 'wind32' ? 'windows-icon.png' : 'icon-template.png';
     const iconPath = path.join(__dirname,`./src/assets/${iconName}`);
-
-    mainWindow.on('blur', () => {
-        mainWindow.hide();
-    })
 
     tray = new TimerTray(iconPath, mainWindow);
 })
