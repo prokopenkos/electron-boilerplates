2d946d268bb0b7b8177a96d050e40d32affc6028
Prokopenko Ivan <ivan.prokopenko@together.com>
2019-05-23 14:33:12 +0300

    Create TimerTray class, set Tooltip

--- boilerplates/tasky/index.js
+++ boilerplates/tasky/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const electron = require('electron');
 const { app, BrowserWindow, Tray } = electron;
+const TimerTray = require('./app/timer_tray');
 
 let mainWindow;
 let tray;
@@ -18,26 +19,5 @@ app.on('ready', () => {
     const iconName = process.platform === 'wind32' ? 'windows-icon.png' : 'icon-template.png';
     const iconPath = path.join(__dirname,`./src/assets/${iconName}`);
 
-    tray = new Tray(iconPath);
-
-    tray.on("click", (event, bounds) => {
-        const {x,y} = bounds;
-        const {height, width} = mainWindow.getBounds();
-
-        if(mainWindow.isVisible())
-            mainWindow.hide();
-        else{
-            const yPosition = process.platform === 'darwin' ? y : y-height;
-            mainWindow.setBounds({
-                x: x-width/2,
-                y: yPosition,
-                height,
-                width,
-            })
-            mainWindow.show();
-        }
-            
-            
-    })
-
+    tray = new TimerTray(iconPath, mainWindow);
 })
