d26045bdb4873a5fd03784628d2a092d03c9a026
Prokopenko Ivan <ivan.prokopenko@together.com>
2019-05-23 14:54:35 +0300

    Hide icok in dock open toolbars

--- boilerplates/tasky/index.js
+++ boilerplates/tasky/index.js
@@ -19,5 +19,9 @@ app.on('ready', () => {
     const iconName = process.platform === 'wind32' ? 'windows-icon.png' : 'icon-template.png';
     const iconPath = path.join(__dirname,`./src/assets/${iconName}`);
 
+    mainWindow.on('blur', () => {
+        mainWindow.hide();
+    })
+
     tray = new TimerTray(iconPath, mainWindow);
 })
